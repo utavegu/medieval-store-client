d3eb79d7f99a9615f21af9c688eda54f806f3a0f
Utavegu <utavegu@list.ru>
2023-09-15 14:23:14 +0500

    chore: wrap app global mobx provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import MobxProvider from '@/store/context';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MobxProvider>{children}</MobxProvider>
+      </body>
     </html>
   );
 }
