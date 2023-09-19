aa78a38801184f350ae8b4a79318eabe41e3caf6
Utavegu <utavegu@list.ru>
2023-09-19 10:45:07 +0500

    feat: add fonts and base styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import MobxProvider from '@/store/context';
-import './globals.css';
-import { Inter } from 'next/font/google';
+// import './globals.css';
+import '../styles/index.css';
+// import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      {/* <body className={inter.className}> */}
+      <body>
         <MobxProvider>{children}</MobxProvider>
       </body>
     </html>
